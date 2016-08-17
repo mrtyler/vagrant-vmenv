5b4a577f2784b0a69b7c0b42116712770cb69b68
Alfredo Matas <amatas@gmail.com>
2016-08-17 17:48:35 +0200

    Merged code and made some changes to allow several vms and applications

--- lib/config_provision.rb
+++ lib/config_provision.rb
@@ -3,14 +3,16 @@
 
 require 'yaml'
 
-def config_provision(instance, vm_config, vm_id)
+def config_provision(instance, vm_config, vm_id, apps)
 
   basecmd = "sudo PYTHONUNBUFFERED=1 VM_HOSTNAME=#{vm_id} "\
             "ansible-playbook /vagrant/#{VAGRANT_VMENV_PATH}/provisioning/base-playbook.yml"
   instance.vm.provision "shell", inline: basecmd
- 
-  vm_config["apps"].each do |app, config|
 
+  apps.each do |config|
+    app = config["app_name"] #app_name
+
+    puts "Up with #{app}"
     #check if every file is in place
     raise "File vagrant/provisioning/#{app}-requirements.yml doesn't exist"\
       unless File.file?(VAGRANT_VMENV_PATH + "/provisioning/#{app}-requirements.yml")
@@ -24,17 +26,17 @@ def config_provision(instance, vm_config, vm_id)
     cmds = \
     "sudo ansible-galaxy install -fr /vagrant/#{VAGRANT_VMENV_PATH}/provisioning/#{app}-requirements.yml \n"\
     "sudo VARS_FILE=#{app}-vagrant-vars.yml PYTHONUNBUFFERED=1 \\\n"
-    config.each do |name, value|
-      cmds << name.upcase + "=" + value.to_s + " \\\n"
-    end
-    if config["deploy"] then
+    # config.each do |name, value|
+    #   cmds << name.upcase + "=" + value.to_s + " \\\n"
+    # end
+    if config["deploy"] then #app_start_service
       cmds << "ansible-playbook --tags='install,configure,deploy' /vagrant/#{VAGRANT_VMENV_PATH}/provisioning/#{app}-playbook.yml"
     else
       cmds << "ansible-playbook --tags='install,configure' /vagrant/#{VAGRANT_VMENV_PATH}/provisioning/#{app}-playbook.yml"
     end
     instance.vm.provision "shell", inline: cmds
 
-  end if vm_config["apps"] and ARGV[0] == "up"
+  end if apps and ARGV[0] == "up"
 
   vm_config["provision"].each do |type, args|
     if type == "shell" then
@@ -46,18 +48,17 @@ def config_provision(instance, vm_config, vm_id)
     end
   end if vm_config["provision"]
 
-  qi_vars = YAML.load(File.read(".qi.yml")) if File.file?(".qi.yml")
-
-  if !qi_vars.nil? and ARGV[0] == "provision" and vm_config["tests_here"] then
-    qi_shellcmds = "cd /home/vagrant/sync \n" 
-    qi_vars["setup"].each do |cmd|
-      qi_shellcmds << cmd + "\n"
-    end unless qi_vars["setup"].nil?
-    qi_vars["commands"].each do |cmd|
-      qi_shellcmds << "DISPLAY=:0 " + cmd + "\n"
-    end unless qi_vars["commands"].nil?
-    puts qi_shellcmds
-    instance.vm.provision "shell", privileged: false, inline: qi_shellcmds
-  end
+  apps.each do |config|
+    if config["run_in"] == vm_id then
+      qi_shellcmds = "cd /home/vagrant/sync \n"
+      config["setup"].each do |cmd|
+        qi_shellcmds << cmd + "\n"
+      end unless config["setup"].nil?
+      config["commands"].each do |cmd|
+        qi_shellcmds << "DISPLAY=:0 " + cmd + "\n"
+      end unless config["commands"].nil?
+      instance.vm.provision "shell", privileged: false, inline: qi_shellcmds
+    end
+  end if apps and ARGV[0] == "provision"
 
 end
